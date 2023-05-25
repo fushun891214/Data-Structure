a644cc4c400d00a450fd2d875cc87059ed86b606
fushun <102985209+fushun891214@users.noreply.github.com>
2023-05-25 07:08:10 +0000

    EXP

---
+++ Exp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int Exp_recursive(int x,int n){
+    if (n==0) return 0;
+    else return x*Exp_recursive(x,n-1);
+}
+
+int Exp_iterative(int x,int n){
+    int f;
+    if ()
+    {
+        /* code */
+    }
+    
+}
+
+int main(){
+
+}

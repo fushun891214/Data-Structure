ce10b80e117bb834ee7c5ca7b05678454458edba
fushun <102985209+fushun891214@users.noreply.github.com>
2023-06-27 09:43:39 +0000

    Exp

--- Exp.cpp
+++ Exp.cpp
@@ -2,18 +2,20 @@
 
 int Exp_recursive(int x,int n){
     if (n==0) return 0;
-    else return x*Exp_recursive(x,n-1);
+    else if (n>1) return x*Exp_recursive(x,n-1);
 }
 
-int Exp_iterative(int x,int n){
+int Exp_recursive_LogN(int x,int n){
     int f;
-    if ()
-    {
-        /* code */
-    }
-    
+    if (n%2==0) f=1;
+    else f=x;
+    if(n<=1) return f;
+    return f*Exp_recursive_LogN(x*x,n/2);
 }
 
 int main(){
-
-https://fushun891214-ubiquitous-eureka-q5599774jpw2xv7r.github.dev/}
+    int x = 2;
+    int n = 7;
+    std::cout << Exp_recursive(x,n) << std::endl;
+    std::cout << Exp_recursive_LogN(x,n) << std::endl;
+}

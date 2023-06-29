545ff25b88e27b69e3a5df875cc3444c0461f6f2
fushun <102985209+fushun891214@users.noreply.github.com>
2023-06-29 07:28:01 +0000

    BinarySearch

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 #include<stdio.h>
 
+int BinarySearch_Recursive(int arr[],int l,int r,int x){
+    if (l<=r){
+        int mid = (l+r)/2;
+        if (arr[mid]==x)
+            return mid;
+        else if (arr[mid]>x)
+            return BinarySearch_Recursive(arr,l,mid-1,x);
+        else
+            return BinarySearch_Recursive(arr,mid+1,r,x);
+    }
+}
+
+int main(){
+    int arr [5] = {1,2,3,4,5};
+    int l = 0;
+    int r = sizeof(arr)/sizeof(arr[0]);
+    std :: cout << BinarySearch_Recursive(arr,l,r,5) << std ::endl;
+}
+
 int BinarySearch_Iterative(int arr[], int l, int r, int x) {
     while (l <= r) {
         int m = (l+r) / 2;
@@ -12,11 +31,4 @@ int BinarySearch_Iterative(int arr[], int l, int r, int x) {
             r = m - 1;
     }
     return -1;
-}
-
-int main(){
-    int arr [5] = {1,2,3,4,5};
-    int l = 0;
-    int r = sizeof(arr)/sizeof(arr[0]);
-    std :: cout << BinarySearch_Iterative(arr,l,r,5) << std ::endl;
 }
